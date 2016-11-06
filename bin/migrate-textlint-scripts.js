faa2fcdf6c746f096b932441c0417e9aecdd0bf9
azu <azuciao@gmail.com>
2016-11-06 21:46:31 +0900

    Initial commit

---
+++ bin/migrate-textlint-scripts.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const shelljs = require("shelljs");
+const npeBin = require.resolve(".bin/npe");
+const echo = function(message) {
+    console.info(message);
+};
+const exec = function(command) {
+    echo("Run: " + command);
+    if (shelljs.exec(command).code !== 0) {
+        throw new Error("Fail to excute:" + command);
+    }
+};
+const npe = function(key, value) {
+    exec(`${npeBin} ${key} ${value}`);
+};
+// Exist config files
+const babelrcPath = path.join(process.cwd(), ".babelrc");
+const mochaOptPath = path.join(process.cwd(), "test", "mocha.opts");
+if (fs.existsSync(babelrcPath)) {
+    echo(`rm ${babelrcPath}`);
+    shelljs.rm(babelrcPath);
+}
+if (fs.existsSync(babelrcPath)) {
+    echo(`rm ${mochaOptPath}`);
+    shelljs.rm(mochaOptPath);
+}
+// Remove exist devDependencies
+const removeDevDeps = [
+    "babel-cli",
+    "babel-preset-es2015",
+    "babel-preset-latest",
+    "babel-register",
+    "mocha",
+    "textlint-tester"
+];
+exec(`npm uninstall --save-dev ${removeDevDeps.join(" ")}`);
+// Install textlint-scripts
+exec(`npm install --save-dev textlint-scripts`);
+// Modify package.json
+npe("scripts.build", "textlint-scripts build");
+npe("scripts.watch", "textlint-scripts build --watch");
+npe("scripts.test", "textlint-scripts test");
+// Complete
+
+echo("✔ Complete!");
---
+++ test/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eux
+declare currentDir=$(cd $(dirname $0);pwd)
+declare dirName=$(basename "${currentDir}")
+declare parentDir=$(dirname "${currentDir}")
+declare exampleDir="${parentDir}/example"
+# rm
+rm -rf ${currentDir}/clone-tmp
+# test
+git clone https://github.com/azu/textlint-rule-no-empty-section.git ${currentDir}/clone-tmp
+cd ${currentDir}/clone-tmp
+git checkout -b 1.1.0 refs/tags/1.1.0
+node ${parentDir}/bin/migrate-textlint-scripts.js
+# success if exit 0

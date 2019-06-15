a9eaa5e2f70520b5964cf5b7a9398f53ebf304d5
azu <azuciao@gmail.com>
2019-06-15 13:27:35 +0900

    add trap

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,7 @@ declare dirName=$(basename "${currentDir}")
 declare parentDir=$(dirname "${currentDir}")
 declare exampleDir="${parentDir}/example"
 # rm
+trap "rm -rf ${currentDir}/clone-tmp" 0 1 2 3 15
 rm -rf ${currentDir}/clone-tmp
 # test
 git clone https://github.com/azu/textlint-rule-no-empty-section.git ${currentDir}/clone-tmp
@@ -12,4 +13,3 @@ cd ${currentDir}/clone-tmp
 git checkout -b 1.1.0 refs/tags/1.1.0
 node ${parentDir}/bin/migrate-textlint-scripts.js
 # success if exit 0
-rm -rf ${currentDir}/clone-tmp
